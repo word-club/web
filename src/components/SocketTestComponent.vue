53459b951643ef7b07f1530a96e2208d73f63510
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-10-20 19:42:42 +0545

    separated socket poc and cleaned home component

---
+++ src/components/SocketTestComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="random-int">{{ notification }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SocketTestComponent',
+  data() {
+    return {
+      websocket: null,
+      notification: null
+    }
+  },
+  created() {
+    this.websocket = new WebSocket('ws://localhost:8000/ws/notification/')
+
+    this.websocket.onmessage = function (event) {
+      this.notification = JSON.parse(event.data)
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    Magic
-  </div>
+  <div />
 </template>
 
 <script>
