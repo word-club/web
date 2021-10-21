150102c9d46e383c0f1b01ffe033bf11d7842d1d
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-10-21 21:33:26 +0545

    Added prototypes and use moment

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import Vue from "vue"
-import App from "./App.vue"
+import App from "./App"
 import router from "./router"
 import store from "./store"
 import vuetify from "./plugins/vuetify"
 import "roboto-fontface/css/roboto/roboto-fontface.css"
 import "@mdi/font/css/materialdesignicons.css"
+import moment from "moment"
+
+import AXIOS from "@/axios"
+import helper from "@/helper"
 
 Vue.config.productionTip = false
+
+Vue.use(moment)
+
+Vue.prototype.$axios = AXIOS
+Vue.prototype.$moment = moment
+Vue.prototype.$helper = helper
+Vue.prototype.$urls = require("@/urls.json")
+
+
 
 new Vue({
 	router,
