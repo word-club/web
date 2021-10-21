94b331bbfbf1290292ba17633fbc2a208d5e252d
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-10-21 21:38:17 +0545

    Added scroll top and snack component

---
+++ src/components/utils/TheSnackbar.vue
@@ -0,0 +1,42 @@
+<template>
+	<v-snackbar
+		v-model="snack"
+		:timeout="snackTimeout"
+	>
+		{{ snackText }}
+
+		<template #action="{attrs}">
+			<v-btn
+				:color="snackColor"
+				text
+				v-bind="attrs"
+				@click="snack=false"
+			>
+				Close
+			</v-btn>
+		</template>
+	</v-snackbar>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+	name: "TheSnackbar",
+	computed: {
+		...mapGetters({
+			snackText: "snack/text",
+			snackColor: "snack/color",
+			snackTimeout: "snack/timeout",
+		}),
+		snack: {
+			get() {
+				return this.$store.state.snack.snack
+			},
+			set(v) {
+				this.$store.dispatch("snack/setSnackState", v)
+			}
+		},
+	},
+}
+</script>
