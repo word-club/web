8056f2ad8c2a08066c9b55ae4bc06ce79af0ca04
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-10-21 21:37:55 +0545

    Init home page top components

---
+++ src/views/home/components/AddPostBox.vue
@@ -0,0 +1,46 @@
+<template>
+	<v-card outlined>
+		<v-card-title class="pa-3">
+			<v-avatar size="40"
+				color="grey darken-2"
+				class="mr-2"
+			/>
+			<v-responsive class="px-2"
+				max-width="1200"
+			>
+				<v-text-field
+					dense outlined
+					hide-details
+					placeholder="Create Post"
+					full-width
+				>
+					<template #prepend-inner>
+						<v-icon size="22">
+							mdi-home
+						</v-icon>
+					</template>
+				</v-text-field>
+			</v-responsive>
+			<v-btn icon>
+				<v-icon>mdi-image</v-icon>
+			</v-btn>
+			<v-btn icon>
+				<v-icon>mdi-attachment</v-icon>
+			</v-btn>
+		</v-card-title>
+	</v-card>
+</template>
+
+<script>
+export default {
+	name: "AddPostBox",
+	props: {},
+	data: () => ({}),
+	computed: {},
+	methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/components/HotBar.vue
@@ -0,0 +1,59 @@
+<template>
+	<v-card outlined>
+		<v-card-actions class="actions pa-3">
+			<v-btn rounded
+				depressed
+			>
+				<v-icon left>
+					mdi-rocket
+				</v-icon>
+				Best
+			</v-btn>
+			<v-btn rounded
+				depressed
+			>
+				<v-icon left>
+					mdi-fire
+				</v-icon>
+				Hot
+			</v-btn>
+			<v-btn rounded
+				depressed
+			>
+				<v-icon left>
+					mdi-white-balance-sunny
+				</v-icon>
+				New
+			</v-btn>
+			<v-btn rounded
+				depressed
+			>
+				<v-icon left>
+					mdi-arrow-up-bold-outline
+				</v-icon>
+				Top
+			</v-btn>
+			<v-btn icon>
+				<v-icon>mdi-dots-horizontal</v-icon>
+			</v-btn>
+			<v-spacer />
+			<v-btn icon>
+				<v-icon>mdi-view-agenda</v-icon>
+			</v-btn>
+		</v-card-actions>
+	</v-card>
+</template>
+
+<script>
+export default {
+	name: "HotBar",
+	props: {},
+	data: () => ({}),
+	computed: {},
+	methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
