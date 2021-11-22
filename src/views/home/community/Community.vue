8f226ef8043f06151c2bb3f85e561933903d005e
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-11-22 13:01:26 +0545

    Community detail page restructuring

--- src/views/home/community/Community.vue
+++ src/views/home/community/Community.vue
@@ -17,62 +17,7 @@
 					/>
 				</v-card>
 			</v-card-text>
-			<v-card-title class="pa-0 white">
-				<v-card max-width="800"
-					width="800"
-					class="mx-auto"
-					flat color="white"
-				>
-					<v-card-text class="pa-3 pt-0 pb-1 d-flex align-center"
-						style="width: 100%"
-					>
-						<v-avatar
-							class="community-avatar"
-							size="80"
-							:color="(community.avatar) ? 'white' : community.theme.color"
-						>
-							<v-img v-if="community.avatar"
-								:src="$link(community.avatar.image)"
-							/>
-						</v-avatar>
-						<div class="px-2" />
-						<div class="px24">
-							{{ community.name }}
-						</div>
-						<v-spacer />
-						<div class="px-2" />
-						<v-btn
-							v-if="community.my_status && community.my_status.is_approved"
-							rounded depressed :color="community.theme.color"
-							@click="unSubscribe"
-						>
-							Joined
-						</v-btn>
-						<v-btn
-							v-else outlined rounded
-							:color="community.theme.color"
-							@click="subscribe"
-						>
-							Join
-						</v-btn>
-						<div class="px-2" />
-						<v-btn
-							v-if="community.my_status && !community.my_status.disable_notification"
-							icon :color="community.theme.color"
-							@click="disableNotification"
-						>
-							<v-icon>mdi-bell-outline</v-icon>
-						</v-btn>
-						<v-btn
-							v-else icon :color="community.theme.color"
-							@click="enableNotification"
-						>
-							<v-icon>mdi-bell</v-icon>
-						</v-btn>
-					</v-card-text>
-					<community-tab />
-				</v-card>
-			</v-card-title>
+			<detail-title />
 			<v-card-text class="pt-4">
 				<v-card max-width="800"
 					class="mx-auto"
@@ -93,10 +38,10 @@
 import {mapGetters, mapMutations} from "vuex";
 import RouteMixin from "@/mixin/RouteMixin.js";
 import FetchMixin from "@/mixin/FetchMixin.js";
-import CommunityTab from "@/views/home/community/CommunityTab.vue";
+import DetailTitle from "@/views/home/community/DetailTitle.vue";
 export default {
 	name: "Community",
-	components: {CommunityTab},
+	components: {DetailTitle},
 	mixins: [RouteMixin, FetchMixin],
 	data: () => ({
 		model: "community"
@@ -126,21 +71,7 @@ export default {
 				tab.addEventListener("mouseover", onMouseOver)
 				tab.addEventListener("mouseout", onMouseOut)
 			})
-		},
-		unSubscribe() {},
-		subscribe() {},
-		disableNotification() {},
-		enableNotification() {},
+		}
 	}
 }
 </script>
-
-<style scoped lang="scss">
-.community-avatar {
-	border: 4px solid white !important;
-	margin-top: -10px;
-}
-.px24 {
-	font-size: 24px;
-}
-</style>
