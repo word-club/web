c9aa696fb04f8f4e6b08f50067a3e05293ec5792
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-12-01 01:38:27 +0545

    Added backend url in env before build

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export function generateFullLink(link) {
 	if (link.startsWith("http://")) return link
 	else if (link.startsWith("https://")) return link
-	else return "http://localhost:8000" + link
+	else return process.env.VUE_APP_BACKEND_HOST + link
 }
