2ca28db087ad4ac9ce97b7904939208aa3d1cb57
Parajuli Kiran <kiranparajuli589@gmail.com>
2022-01-31 20:45:33 +0545

    updated dist

---
+++ dist/js/chunk-2d0c1416.5f4e9b78.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c1416"],{"44ee":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},o=[],u={name:"ModMail",props:{},data:function(){return{}},computed:{},methods:{}},a=u,r=t("2877"),s=Object(r["a"])(a,c,o,!1,null,"3c2c488b",null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0c1416.5f4e9b78.js.map
