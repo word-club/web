94c229a920413d26944e7c391d1b1d17fdfea78a
Parajuli Kiran <kiranparajuli589@gmail.com>
2022-02-17 00:42:09 +0545

    build bot: updated dist

--- dist/js/chunk-2d0c2145.4217e2c4.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c2145"],{"497a":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},o=[],a={name:"Notification",props:{},data:function(){return{}},computed:{},methods:{}},u=a,i=t("2877"),r=Object(i["a"])(u,c,o,!1,null,"80672dee",null);n["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0c2145.4217e2c4.js.map
